d8f4075e13cdcabc178468bf02a59c8bbc3c6d9a
Samuel Benning <Bensam1993@gmail.com>
2022-05-31 18:04:59 -0400

    Changed structure of html templates folder to leverage Jinja templating capabilities. There is now a base html file that other files will inherit from. Also brought in bootstrap for easy styling.

--- data/views.py
+++ data/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
 import base64
 from io import BytesIO
 
@@ -8,16 +8,17 @@ views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
-    # Generate the figure **without using pyplot**
-    fig = Figure()
-    ax = fig.subplots()
-    ax.plot([1,2])
-    # Save it to a temporary buffer
-    buf = BytesIO()
-    fig.savefig(buf, format="png")
-    # Embed the result in the html output
-    data = base64.b64encode(buf.getbuffer()).decode("ascii")
-    return f"<img src='data:image/png;base64,{data}'/>"
+    # # Generate the figure **without using pyplot**
+    # fig = Figure()
+    # ax = fig.subplots()
+    # ax.plot([1,2])
+    # # Save it to a temporary buffer
+    # buf = BytesIO()
+    # fig.savefig(buf, format="png")
+    # # Embed the result in the html output
+    # data = base64.b64encode(buf.getbuffer()).decode("ascii")
+    # return f"<img src='data:image/png;base64,{data}'/>"
+    return render_template("home.html")
 
 @views.route('/enter-info')
 def enter_info():
